4250ba22d802f04589397e669b12675344744e4e
Icaro-pavani <icaro.pavani79@gmail.com>
2022-05-20 17:14:55 -0300

    feat: base structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import GlobalStyle from "./assets/theme/GlobalStyle";
+
+import HackathonContext from "./context/HackathonContext";
 
 export default function App() {
   return (
     <>
       <GlobalStyle />
-      <h1>Teste</h1>
+      <HackathonContext.Provider value={{}}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<h1>Teste</h1>} />
+          </Routes>
+        </BrowserRouter>
+      </HackathonContext.Provider>
     </>
   );
 }
